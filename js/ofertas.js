0e93fbad65715586ddcb56c09319e3f48eedaf46
Abirlopez <abigaillopez.r22@gmail.com>
2026-06-15 22:53:24 -0300

    js en vuelos y ofertas

---
+++ js/ofertas.js
@@ -0,0 +1,41 @@
+import { destinos } from "./data/destinosList.js";
+
+const contenedor = document.querySelector(".contenedor-ofertas");
+
+destinos.forEach(oferta => {
+    contenedor.innerHTML += `
+    
+         <div class="tarjeta">
+
+
+                <span class="promo">PROMO</span>
+                <img src="${oferta.imagen}" alt="${oferta.destino}">
+
+                <div class="info">
+                
+                    <div class="parte-arriba">
+                        <h3>${oferta.destino}</h3>
+                        <p>${oferta.pais}</p>
+                    </div>
+
+                    <div class="parte-abajo">
+                        <p>Desde</p>
+                        <p class="precio">US$${oferta.precio}</p>
+
+                        <div class="ver-vuelos">
+                            <a href="../pages/detalleDestino.html?destino=${oferta.destino}">
+                                <p>Ver Oferta</p>
+                            </a>
+
+                            <a href="../pages/detalleDestino.html?destino=${oferta.destino}">
+                                <img src="../images/flecha-naranja.png" alt="flecha">
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+    `;
+});                                
